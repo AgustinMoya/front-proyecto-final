f48561f8c3379e06e5cf1f64a67973964d230942
Agustin Moya <agustin.moya@despegar.com>
2019-08-26 08:34:40 -0300

    Se protegen las rutas de usuarios no authorizados

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { withAuthorization } from "../Session";
+const HomePage = () => (
+  <div>
+    <h1>Página principal</h1>
+    <p>La página de "Home" solo puede ser vista por usuarios logueados</p>
+  </div>
+);
 
-const Home = () => <span>Home</span>;
-
-export default Home;
+//TODO: Chequea que no sea null, cambiar por rol de usuario si se implementa alguna pagina solo para ADMIN
+const condition = authUser => !!authUser;
+export default withAuthorization(condition)(HomePage);
