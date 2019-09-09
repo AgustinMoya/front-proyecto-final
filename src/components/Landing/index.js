0dd148c41b3c5afe5860b2e51c1eab1460cfc1a3
Agustin Moya <agustin.moya@despegar.com>
2019-09-09 09:24:12 -0300

    Add Jumbotron component to landing

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,5 +1,17 @@
 import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
 
-const Landing = () => <span>adssad</span>
+const Landing = () => (
+  <Jumbotron fluid>
+    <Container>
+      <h1>Bienvenidos al proyecto final del Team wall-e</h1>
+      <p>
+        Este es el frontend de la aplicación, usted podrá realizar diferentes acciones dependiendo
+          si es un usuario o un administrador.
+      </p>
+    </Container>
+  </Jumbotron>
+);
 
 export default Landing;
