66b89b41956fffb33843d491aafec76d1a9eec3d
Matias Fernandez <mfernandez@guidewire.com>
2019-10-09 20:35:44 -0300

    New Version Home Page Static

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,55 @@
 import React from "react";
 import { withAuthorization } from "../Session";
 const HomePage = () => (
-  <div>
-    <h1>Página principal</h1>
-    <p>La página de "Home" solo puede ser vista por usuarios logueados</p>
+  <div class="container">
+    <h1>Bienvenidos Nacho</h1>
+    <div class="row">
+      <div class="col-sm-8">
+        <div class="card">
+          <h5 class="card-header">Tabla pedidos</h5>
+          <div class="card-body">
+          <div class="table-responsive">
+
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Numero Articulo</th>
+                  <th scope="col">Producto</th>
+                  <th scope="col">Estado</th>
+                  <th scope="col">Ver</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>123814</td>
+                  <td>Iphone</td>
+                  <td>En proceso</td>
+                  <td><a href="#" class="btn btn-primary">Go somewhere</a></td>
+                </tr>
+              </tbody>
+            </table>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-4">
+        <div class="card">
+          <h5 class="card-header">Acciones</h5>
+          <div class="card-body">
+            <div class="card" >
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Buscar torre</li>
+                <li class="list-group-item">Ver deposito</li>
+                <li class="list-group-item">Ver robots</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
