e7225cbeb256bf567cb565964984d279d4e9662b
Agustin Moya <agustin.moya@despegar.com>
2019-11-18 14:12:14 -0300

    Fix matriz

--- src/components/Deposit/ReadOnlyDeposit/index.js
+++ src/components/Deposit/ReadOnlyDeposit/index.js
@@ -7,14 +7,14 @@ const ReadOnlyDeposit = ({ matrix }) => {
   const createDeposit = matrix => {
     let depositValue = [];
 
-    for (let i = 0; i <= matrix.length - 1; i++) {
-      for (let j = 0; j <= matrix[i].length - 1; j++) {
+    for (let i = 0; i < matrix.length ; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
         const seatingStyle = (
           <ReadOnlyEstante column={j} row={i} value={matrix[i][j]} />
         );
         depositValue.push(seatingStyle);
 
-        if (j === matrix[i].length - 1 - 1) {
+        if (j === matrix[i].length - 1) {
           const seatClear = <div className="clearfix" />;
           depositValue.push(seatClear);
         }
