b6cec9d2ce122c25988afc70148c2955bccb39f7
Agustin Moya <agustin.moya@despegar.com>
2019-11-19 00:32:38 -0300

    Delete robot

--- src/components/Robots/index.js
+++ src/components/Robots/index.js
@@ -12,6 +12,9 @@ class RobotForm extends Component {
     this.state = {
       createRobotMessage: null,
       createRobotCode: null,
+      deleteRobotMessage: null,
+      deleteRobotCode: null,
+      robotToDelete: "",
       robots: []
     };
   }
@@ -38,12 +41,35 @@ class RobotForm extends Component {
         });
       });
   };
+
+  deleteRobot = idRobot => {
+    ApiClient.deleteRobot(idRobot)
+      .then(({ data, status }) => {
+        this.setState({
+          deleteRobotMessage: data,
+          deleteRobotCode: status
+        });
+      })
+      .catch(error => {
+        this.setState({
+          deleteRobotMessage: error.response.data,
+          deleteRobotCode: error.response.status
+        });
+      });
+  };
   onChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
   render() {
-    const { createRobotMessage, createRobotCode, robots } = this.state;
+    const {
+      createRobotMessage,
+      createRobotCode,
+      deleteRobotMessage,
+      deleteRobotCode,
+      robots,
+      robotToDelete
+    } = this.state;
     const posicion = localStorage.getItem("posicion");
     const platform = localStorage.getItem("platform");
     return (
@@ -56,30 +82,52 @@ class RobotForm extends Component {
         <hr></hr>
         <Row>
           <Col xs={6}>
-            <div>
-              <div class="form-row">
-                <div class="col">
-                  <span>{`Usted se logueo a la plataforma: ${platform}, el robot se creara en la posicion: ${posicion}`}</span>
-                </div>
-                <div class="col">
-                  <center>
-                    <button
-                      type="submit"
-                      class="btn btn-primary"
-                      onClick={() => this.createRobot(posicion)}
-                    >
-                      Crear robot
-                    </button>
-                  </center>
-                </div>
-              </div>
+            <div class="form-group">
+              <span>{`Usted se logueo a la plataforma: ${platform}, el robot se creara en la posicion: ${posicion}`}</span>
             </div>
+            <button
+              type="submit"
+              class="btn btn-primary"
+              onClick={() => this.createRobot(posicion)}
+            >
+              Crear robot
+            </button>
           </Col>
           <Col xs={6}>
             {createRobotCode >= 200 && createRobotCode < 300 ? (
               <Alert variant="success">{createRobotMessage}</Alert>
             ) : createRobotCode === 500 ? (
               <Alert variant="danger">{createRobotMessage}</Alert>
+            ) : null}
+          </Col>
+        </Row>
+        <hr></hr>
+        <Row>
+          <Col xs={6}>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Eliminar robot</label>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Id del robot a eliminar"
+                name="robotToDelete"
+                value={robotToDelete}
+                onChange={this.onChange}
+              />
+            </div>
+            <button
+              type="submit"
+              class="btn btn-primary"
+              onClick={() => this.deleteRobot(robotToDelete)}
+            >
+              Eliminar robot
+            </button>
+          </Col>
+          <Col xs={6}>
+            {deleteRobotCode >= 200 && deleteRobotCode < 300 ? (
+              <Alert variant="success">{deleteRobotMessage}</Alert>
+            ) : deleteRobotCode === 500 ? (
+              <Alert variant="danger">{deleteRobotMessage}</Alert>
             ) : null}
           </Col>
         </Row>
